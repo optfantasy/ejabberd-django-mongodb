4b19266f7912479bcca59badeb0ffa9fa6d22188
LittleQ <littleq0903@gmail.com>
2013-02-13 16:18:20 +0800

    too lazy to write commit messages

--- scripts/gen_slave_setting.sh
+++ scripts/gen_slave_setting.sh
@@ -1,15 +1,19 @@
+#!/bin/bash
 if [ -z $1 ]; then
-	echo "must specify the number of slave servers." >&2
-	echo "Usage: scripts/gen_slave_setting.sh <slave_num>" >&2
+	echo "must specify the info of slave servers." >&2
+	echo "Usage: scripts/gen_slave_setting.sh <slave_hostname>" >&2
 	exit 1;
 fi 
 
-SLAVE_NUM=$1
+SLAVE_HOST=$1
 
-echo "Generating $SLAVE_NUM templates for slave servers..."
+echo "Generating templates for slave servers..."
 
-for num in $(seq 1 $SLAVE_NUM)
-do
-	echo make generate_setting IN_TMPL=slave OUT_TMPL=xmpp-slave-$num
-	make generate_setting IN_TMPL=slave OUT_TMPL=xmpp-slave-$num
-done
+# Generate the configuration file
+echo make generate_setting IN_TMPL=slave OUT_TMPL=${SLAVE_HOST}
+make generate_setting IN_TMPL=slave OUT_TMPL=${SLAVE_HOST}
+
+# Modify the hostname
+echo "Modifying the hostname in configuration..."
+sed -i "s/{{hostname}}/${SLAVE_HOST}/g" rel/reltool_vars/${SLAVE_HOST}_vars.config
+
