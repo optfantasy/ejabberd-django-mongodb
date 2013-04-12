dafa7928db9c836ffabc860b152b46451133d0d3
LittleQ <littleq0903@gmail.com>
2013-04-12 14:33:45 +0800

    new version controlling mechanism

--- scripts/gen_master_setting.sh
+++ scripts/gen_master_setting.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
-if [ -z $1 ]; then
+if [ -z $1 ] || [ -z $2 ]; then
 	echo "must specify the info of master servers." >&2
-	echo "Usage: scripts/gen_master_setting.sh <master_hostname>" >&2
+	echo "Usage: scripts/gen_master_setting.sh <master_hostname> <deploy_version>" >&2
 	exit 1;
 fi 
 
 MASTER_HOST=$1
+DEPLOY_VER=$2
 
 echo "Generating templates for master servers..."
 
 # Generate the configuration file
 echo make generate_setting IN_TMPL=master OUT_TMPL=${MASTER_HOST}
-make generate_setting IN_TMPL=master OUT_TMPL=${MASTER_HOST}
+make generate_setting IN_TMPL=master OUT_TMPL=${MASTER_HOST} DEPLOY_VER=${DEPLOY_VER}
 
 # Modify the hostname
 echo "Modifying the hostname in configuration..."
