0b32bc5ddf1052b0dae3db260e8a1a84de833beb
ejabberd <ejabberd@xmpp-build>
2013-04-24 20:13:14 -0700

    milestone: functionality work, start refactory

--- scripts/reset_nodes.sh
+++ scripts/reset_nodes.sh
@@ -1,9 +1,9 @@
 ssh xmpp-dev-master-1 "ejabberd/bin/ejabberd stop"
 ssh xmpp-dev-slave-1 "ejabberd/bin/ejabberd stop"
-#ssh xmpp-dev-slave-2 "ejabberd/bin/ejabberd stop"
-#ssh xmpp-dev-slave-3 "ejabberd/bin/ejabberd stop"
+ssh xmpp-dev-slave-2 "ejabberd/bin/ejabberd stop"
+ssh xmpp-dev-slave-3 "ejabberd/bin/ejabberd stop"
 
 ssh xmpp-dev-master-1 "rm -rf ejabberd ejabberd-old"
 ssh xmpp-dev-slave-1 "rm -rf ejabberd ejabberd-old"
-#ssh xmpp-dev-slave-2 "rm -rf ejabberd ejabberd-old"
-#ssh xmpp-dev-slave-3 "rm -rf ejabberd ejabberd-old"
+ssh xmpp-dev-slave-2 "rm -rf ejabberd ejabberd-old"
+ssh xmpp-dev-slave-3 "rm -rf ejabberd ejabberd-old"
